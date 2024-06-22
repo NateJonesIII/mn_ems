a561df9e077e70da37fd81f2d3faeccd7bbc3ee8
Nate Jones <15nate.jones@gmail.com>
2024-06-22 00:22:46 -0400

    updating .sh files, documents, folder structure

--- init_db.sh
+++
@@ -1,6 +0,0 @@
-#Create docker-compose file first
-docker-compose up --build
-
-docker-compose exec web flask db init
-docker-compose exec web flask db migrate
-docker-compose exec web flask db upgrade
